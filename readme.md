aedb1d873c7dafdd7ce7b7c1829a9f7be29307ba
caiquesarkis <caiquesarkis@gmail.com>
2024-11-06 23:07:05 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,46 @@
+
+![Kuroi Orochi Clan Logo](assets/kuroi-card-template.png)
+# Kuroi Orochi 🐍 Card: Generator
+
+Esta é uma aplicação baseada na web construída com JavaScript, HTML, jQuery e Bootstrap 6. Ela gera cartões digitais personalizáveis para membros do Clã Kuroi Orochi. Esta ferramenta é ideal para criar e compartilhar rapidamente cartões de clã para comunidades online, jogos de RPG ou uso pessoal.
+
+Características
+Cartões de Clã Personalizáveis: Personalize facilmente os detalhes do membro do clã, como nome, rank, atributos e imagens personalizadas.
+Design Responsivo: Construído com o Bootstrap 6, garantindo que o gerador de cartões seja totalmente responsivo em diferentes dispositivos.
+Pré-visualização ao Vivo: Visualize o cartão em tempo real enquanto personaliza vários elementos.
+Interface Simples e Intuitiva: Desenvolvido com jQuery para garantir uma experiência de usuário suave e interativa.
+Demonstração
+
+Instalação
+Clone o Repositório:
+
+```bash
+git clone https://github.com/yourusername/kuroi-orochi-card-generator.git
+cd kuroi-orochi-card-generator
+
+```
+
+Abra o arquivo index.html: Abra o arquivo index.html no seu navegador para acessar o gerador localmente.
+
+Alternativamente, use o Live Server:
+
+Instale a extensão Live Server (se estiver usando o Visual Studio Code).
+Abra o index.html com o Live Server para recarregar automaticamente as alterações.
+
+## Uso do aplicativo
+Preencha os Detalhes do Membro: Use os campos de entrada para inserir o nome, rank e outros atributos pessoais do membro. Selecione ou Envie Imagens: Adicione uma imagem para o membro do clã a partir de um arquivo ou URL. Personalize os Estilos: Ajuste as cores, fontes e layout para combinar com a estética do clã. Gere e Baixe: Quando estiver satisfeito com o design, gere e baixe o cartão como uma imagem.
+
+
+## Tecnologias Usadas
+- JavaScript: Para geração dinâmica e interativa dos cartões. 
+- HTML: Para estruturar o layout do gerador de cartões. 
+- jQuery: Para manipulação do DOM e interações suaves com o usuário. 
+- Bootstrap 6: Para elementos de design responsivos e elegantes.    
+
+## Contribuições
+Contribuições Contribuições são bem-vindas! Abra um problema ou envie um pull request com suas melhorias ou novos recursos.
+
+Licença Este projeto é licenciado sob a Licença MIT. Veja LICENSE para mais informações.
+
+Contato Para dúvidas ou feedback, entre em contato com Caique Sarkis.
+
---
+++ scripts/image/image.js
@@ -0,0 +1,104 @@
+// Use jQuery to select elements
+const $canvas = $('#canvas');
+const canvas = $canvas[0];
+const ctx = canvas.getContext('2d');
+const $uploadInput = $('#upload');
+const $downloadButton = $('#download');
+const $atkLabel = $('#atk');
+const $defLabel = $('#def');
+const $velLabel = $('#vel');
+
+
+
+// Project config
+
+const CanvasConfig = {
+    width: 343,//272,
+    height: 500
+}
+
+// DrawImage function
+const drawImage = (image, x = 0, y = 0, width = canvas.width, height = canvas.height) => {
+    if (typeof image === "string") {
+        // If `image` is a URL, create an Image object and load it
+        const img = new Image();
+        img.src = image;
+        img.onload = () => {
+            ctx.drawImage(img, x, y, width, height);
+        };
+    } else if (image instanceof HTMLImageElement) {
+        // If `image` is an Image object, draw it directly
+        image.onload = () => {
+            ctx.clearRect(0, 0, width, height); // Optional: Clear canvas before drawing
+            ctx.drawImage(image, x, y, width, height);
+        };
+        // Ensure the image is loaded before drawing
+        if (image.complete) {
+            ctx.drawImage(image, x, y, width, height);
+        }
+    }
+};
+
+// DrawText function
+const drawText = (text, x = 0, y = 0, fontSize = 20, color = 'black') => {
+    // Set font and color for the text
+    ctx.font = `${fontSize}px Arial`;
+    ctx.fillStyle = color;
+
+    // Draw the text on the canvas at specified coordinates
+    ctx.fillText(text, x, y);
+};
+
+// Draw initial template images
+drawImage('../assets/kuroi-card-template.png');
+drawImage('../assets/kuroi-logo-60.png', 17.5, 17.5, 80, 80); // Draw logo
+
+// Handle Image Upload
+$uploadInput.on('change', (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const image = new Image();
+        image.src = URL.createObjectURL(file);
+        image.onload = () => {
+            const offset = 278
+        drawImage(image, (canvas.width - offset)/2, (canvas.height- offset)/2 - 35, offset, offset + 35); // Draw at top-left corner
+
+        drawImage('../assets/kuroi-logo-60.png', 17.5, 17.5, 80, 80); // Draw logo
+        };
+    }
+});
+
+// Handle Canvas Download
+$downloadButton.on('click', () => {
+    const link = document.createElement('a');
+    link.download = 'canvas-image.png';
+    link.href = canvas.toDataURL();
+    link.click();
+});
+
+$('#atk').on('input', (event) => {
+    const atkValue = event.target.value;
+    
+    drawText(`ATK: ${atkValue}`, canvas.width*0.26 - 45, canvas.height* 0.94, 14, 'black');
+});
+
+$('#def').on('input', (event) => {
+    const atkValue = event.target.value;
+    
+    drawText(`DEF: ${atkValue}`, canvas.width*0.26 + 45, canvas.height* 0.94, 14, 'black');
+});
+
+$('#vel').on('input', (event) => {
+    const atkValue = event.target.value;
+    
+    drawText(`VEL: ${atkValue}`, canvas.width*0.26 + 135, canvas.height* 0.94, 14, 'black');
+});
+
+$('#nome').on('input', (event) => {
+    const nomeValue = event.target.value;
+    
+    drawText(`${nomeValue}`, canvas.width*0.3, canvas.height* 0.14, 30, 'black');
+});
+
+
+
